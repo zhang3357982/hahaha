11ecb8c494cbcb5a4b6d7b20564eda6bd2aa2fcf
lkjy <lkjy@shangdan1v2mac-mini.local>
2016-05-04 19:13:04 +0800

    FDSDF

---
+++ GUF/GUF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GUF
+//
+//  Created by lkjy on 16/5/4.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GUF/GUF/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GUF
+//
+//  Created by lkjy on 16/5/4.
+//  Copyright © 2016年 张涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"FGSDE");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
